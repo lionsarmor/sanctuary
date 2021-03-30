d9fb8d00925feb00cebd00c7800dbe60a4a3e13d
james weeks <james.m.weeks@gmail.com>
2021-03-30 00:02:46 -0700

    database up

--- mongoDB-Sanctuary/server.js
+++ mongoDB-Sanctuary/server.js
@@ -1,10 +1,72 @@
 const express = require('express');
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express();
+const connectionString = 'mongodb+srv://sanctuary:@Password1@cluster0.gs4uu.mongodb.net/sanctuary?retryWrites=true&w=majority'
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
-
-  app.get('/', (req, res) => {
-    res.send('Hello World')
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('sanctuary-catalog')
+    const dataCollection = db.collection('data')
+    //set
+    app.set('view engine', 'ejs')
+    //use
+    app.use(express.static('public'))
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    //get
+    app.get('/', (req, res) => {
+      db.collection('data').find().toArray()
+        .then(results => {
+          res.render('index.ejs', { data : results })
+        })
+        .catch(error => console.error(error))
+    })
+    //post
+    app.post('/data', (req, res) => {
+      dataCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+    
+    //delete
+    app.delete('/data', (req, res) => {
+      dataCollection.deleteOne(
+        { name: req.body.name },
+      )
+      .then(result => {
+        if (result.deletedCount === 0) {
+          return res.json('No quote to delete')
+        }
+        res.json(`Deleted Darth Vadar's quote`)
+      })
+      .catch(error => console.error(error))
+    })
+    
+    //put
+    app.put('/data', (req, res) => {
+      dataCollection.findOneAndUpdate(
+        { name: 'Yoda' },
+        {
+          $set: {
+            name: req.body.name,
+            address: req.body.quote
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+      .then(result => {
+        res.json('Success')
+      })
+      .catch(error => console.error(error))
+    })
+    //listen
+    app.listen(3000, function() {
+      console.log('listening on 3000')
+    })
   })
