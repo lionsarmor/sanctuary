3464ca8febd4b33b33f07ee0a57abb06a9acc964
james weeks <james.m.weeks@gmail.com>
2021-04-04 15:41:12 -0700

    server edit

--- server.js
+++ server.js
@@ -67,7 +67,6 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
       .catch(error => console.error(error))
     })
     //listen
-    app.listen(3000, function() {
-      console.log('listening on 3000')
-    })
+    app.listen(process.env.PORT || 3000, 
+      () => console.log("Server is running..."));
   })
